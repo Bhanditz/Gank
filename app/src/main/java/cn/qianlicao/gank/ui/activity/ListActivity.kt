c77f5b3a659f8b0a553b47435fbe6c42bb4d3ec7
Darin <dyy726@qq.com>
2016-03-19 07:26:01 +0800

    打算采用单独页面的方式来显示

---
+++ app/src/main/java/cn/qianlicao/gank/ui/activity/ListActivity.kt
@@ -0,0 +1,16 @@
+package cn.qianlicao.gank.ui.activity
+
+import android.os.Bundle
+
+import cn.qianlicao.gank.R
+
+class ListActivity : BaseActivity() {
+
+    override fun myContentViewId() = R.layout.activity_list
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
